efc8f944b43a4661ea89ba4c42c5e938fbba81de
tye642 <tye642@gmail.com>
2021-11-08 15:24:51 -0600

    changes to interview demo

--- notebook/TY_SQL_Code.sql
+++ notebook/TY_SQL_Code.sql
@@ -90,7 +90,7 @@ WHERE yearid = 2016
 GROUP by pos_groups;
 
 /*
-Q5. Find the average number of strikeouts per game by decade since 1920. 
+***Q5. Find the average number of strikeouts per game by decade since 1920. 
 Round the numbers you report to 2 decimal places. 
 Do the same for home runs per game. Do you see any trends?
 */
@@ -134,6 +134,143 @@ Doing this will probably result in an unusually small number of wins for a world
 Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? 
 What percentage of the time?
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--Q9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+
+/*
+Let's break this one down. What do I need first? Let's find the Manager of the Year awards. 
+*/
+
+--This will UNION all the managers that have gotten the TSN award, first with the NL and then with AL. I think I will need to use a inner join to find managers that won both. 
+SELECT *
+FROM (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'NL' AND awardid = 'TSN Manager of the Year'
+	UNION ALL
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'AL' AND awardid = 'TSN Manager of the Year') sub
+;
+
+/*
+I will use a CTE to define a table for TSN winners for the NL, and another CTE for TSN winners for AL.
+Let's try an INNER JOIN. 
+This gives me a list of managers that have won both TSN awards in the NL and the AL. 
+I will add a LEFT JOIN to include the manager's full name. I will CONCAT their first name and their last name.
+*/
+
+WITH NL AS (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'NL' AND awardid = 'TSN Manager of the Year'),
+	
+	AL AS (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'AL' AND awardid = 'TSN Manager of the Year')
+
+SELECT NL.playerid, CONCAT(p.namefirst, ' ', p.namelast) AS manager_name, NL.yearid AS TSN_NL_year, AL.yearid AS TSN_AL_year
+FROM NL
+INNER JOIN AL
+USING (playerid)
+LEFT JOIN people AS p
+USING (playerid)
+;
+
+/*
+Now I need the teams they were on during those winning years. Tough. Stuck.
+Let's try this with subqueries
+Top portion finds all of the NL TSN and bottom finds all the AL. Subqueries limit only to coaches that have won both. 
+*/
+
+SELECT DISTINCT NL.playerid, CONCAT(p.namefirst, ' ', p.namelast) AS manager_name, nl.yearid AS year, m.teamid, t.name, 'NL' AS league
+FROM (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'NL' AND awardid = 'TSN Manager of the Year') AS NL
+INNER JOIN (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'AL' AND awardid = 'TSN Manager of the Year') AS AL
+USING (playerid)
+LEFT JOIN people AS p
+USING (playerid)
+LEFT JOIN managers AS m
+ON nl.playerid = m.playerid AND nl.yearid = m.yearid
+LEFT JOIN teams AS t
+ON m.teamid = t.teamid AND nl.yearid = t.yearid
+UNION
+SELECT DISTINCT AL.playerid, CONCAT(p.namefirst, ' ', p.namelast) AS manager_name, AL.yearid AS year, m.teamid, t.name, 'AL' AS league
+FROM (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'NL' AND awardid = 'TSN Manager of the Year') AS NL
+INNER JOIN (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'AL' AND awardid = 'TSN Manager of the Year') AS AL
+USING (playerid)
+LEFT JOIN people AS p
+USING (playerid)
+LEFT JOIN managers AS m
+ON al.playerid = m.playerid AND al.yearid = m.yearid
+LEFT JOIN teams AS t
+ON m.teamid = t.teamid AND al.yearid = t.yearid
+ORDER BY league
+; 
+
+/*
+Q11. Is there any correlation between number of wins and team salary? 
+Use data from 2000 and later to answer this question.
+As you do this analysis, keep in mind that salaries across the whole league tend to increase together, so you may want to look on a year-by-year basis.
+*/
+
+--This gives me a teams yearly wins starting in year 2000.
+SELECT teamid, yearid, SUM(w) AS wins_year
+FROM teams
+WHERE yearid >= 2000
+GROUP BY teamid, yearid
+ORDER BY teamid, yearid;
+
+--This gives me the yearly salary starting in the year 2000
+SELECT teamid, yearid, SUM(salary) AS yearly_salary
+FROM salaries
+WHERE yearid >= 2000
+GROUP BY teamid, yearid
+ORDER BY teamid, yearid;
+
+--Let's join them. Added a column for salary per win to help analyze the data. 
+SELECT t.teamid, t.yearid, SUM(t.w) AS wins_year, CONCAT(ROUND(CAST(s.yearly_salary/1000000.00 AS numeric),2), 'M') AS salary_mil, ROUND(CAST(s.yearly_salary/1000000 AS numeric)/SUM(t.w),2) AS salary_mil_per_win
+FROM teams AS t
+LEFT JOIN (
+	SELECT teamid, yearid, SUM(salary) AS yearly_salary
+	FROM salaries
+	WHERE yearid >= 2000
+	GROUP BY teamid, yearid
+	ORDER BY teamid, yearid) AS s
+ON t.teamid = s.teamid AND t.yearid = s.yearid
+WHERE t.yearid >= 2000
+GROUP BY t.teamid, t.yearid, s.yearly_salary
+ORDER BY t.teamid, t.yearid;
+--I can also order by wins or the salary per win column to help conceptulize it.
+
+
 
 
 
